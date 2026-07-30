6654bbae64d141615288e4e364fae3c788c716bf
Norman Leyva Garcia <75862898+nlgarcia84@users.noreply.github.com>
2026-07-30 23:30:28 +0200

    chore: disable astro dev toolbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,9 @@ import vercel from '@astrojs/vercel';
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
+  devToolbar: {
+    enabled: false,
+  },
   vite: {
     plugins: [tailwindcss()],
     server: {
